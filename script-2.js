08bcd7fc21f147982656be66a1452bced3f6c89e
teknosal <77878659+teknosal@users.noreply.github.com>
2021-02-13 01:28:59 +0300

    Update script-2.js
    HW-3

--- script-2.js
+++ script-2.js
@@ -14,14 +14,20 @@ const petsModule = (function(){
             sound: "meow",
             soundText: "Meow - type m"
         }
+      
     ];
     const $tbodyEl = document.querySelector("tbody");
     const $buttons= document.querySelectorAll("button");
+    const mainImage = document.querySelector(".main-image");
+    
+    const getPetRows = function(){
+        return $tbodyEl.querySelectorAll("tr");
+    }
 
     const getButtons = function(){
         return document.querySelectorAll("button");
     }
-
+   
     const createPetElement = function(pet){
         return "<tr><td><img class='pet-image' src='"+pet.image+"' /></td><td>"+pet.name+"</td><td>"+pet.type+"</td><td><button data-sound='"+pet.sound+"'>"+pet.soundText+"</button></td></tr>"
     };
@@ -36,10 +42,35 @@ const petsModule = (function(){
         }
     }
 
+ // Seçilen pet fotoğrafının ekrana gelmesi ve satır renginin değişmesi
+    const selectedPet = function () {
+        const rows = getPetRows();
+        for(let i= 0; i< rows.length; i++){
+            rows[i].addEventListener("click", function(event){
+                mainImage.setAttribute("src", _data[i].image);
+                rows[i].style.background ="blue"
+            });
+        }
+      }
+
+// bark için b, meow için m
+    const playWithKey = function(){
+        document.addEventListener("keydown", function(e){
+            if(e.keyCode == 66){
+                document.getElementById('bark').play()
+            }
+            else if(e.keyCode == 77){
+                document.getElementById('meow').play()
+            }
+        })
+    }
+ 
+
     const bindEvents = function(){
         const buttons = getButtons();
         for(let i= 0; i< buttons.length; i++){
             buttons[i].addEventListener("click", function(event){
+                event.stopPropagation();
                 const soundId = this.dataset.sound;
                 const soundElement = document.getElementById(soundId);
                 if(soundElement){
@@ -52,6 +83,8 @@ const petsModule = (function(){
     const init = function(){
         putPetsInHtml();
         bindEvents();
+        playWithKey();
+        selectedPet();
     }
 
     return {
